dc4c82a14f318b70a40d2b92acb5352137479d05
Gautam <RGautam@iaai.com>
2020-01-22 16:01:23 -0600

    Update for Latest folder

--- FindDirToScan.py
+++ FindDirToScan.py
@@ -7,6 +7,9 @@ def GetScanDirs(fileshare_path):
     # write_to_csv("","ALL_Release_Folders",True)
     lastStr = ""
     currStr  = ""
+    latest_modified_time = 0.0
+    latest_file =""
+    data = ""
     for root, dirs, files in os.walk(str(fileshare_path)):
         File_count = len(files)
         top_folder_pos = root.find(".Release")
@@ -17,14 +20,30 @@ def GetScanDirs(fileshare_path):
                 # print('Root :',root,",",data,",",similar(currStr,lastStr))
                 #prnData = root+","+data+","+str(similar(currStr,lastStr))
                 dist = similar(currStr,lastStr)
+                # file_path = os.path.join(root, fileNm)
+                #file_version = ".".join ([str (i) for i in get_version_number (file_path)])
+                attrib = os.path.getsize(root)
+                modified_time = os.path.getmtime(root)
+                print("root",root,"latest_file =",latest_file , ", mod time =",modified_time)
+                if modified_time>latest_modified_time:
+                    latest_modified_time = modified_time
+                    latest_file = currStr
+
+                
+                
                 if dist  < 0.80:
+                    latest_modified_time = modified_time
                     dataArr=[]
                     root = root.replace("\\"+data,"/"+data)
                     dataArr.append(root)
-                    print(root,dist)
+                    print("dist gt 0.80",root,latest_file)
                     write_to_csv(dataArr,"scan",False)
                 lastStr = currStr  
-           
+    dataArr=[]
+    print(data)
+    latest_file = latest_file.replace("\\","/")
+    dataArr.append(latest_file)
+    write_to_csv(dataArr,"scan",False)       
 
 def similar(a,b):
     return SequenceMatcher(None,a,b).ratio()
